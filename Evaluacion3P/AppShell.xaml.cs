e65da238d53409b689f6b2817dd8c5310bee5a26
José Francisco Sánchez Ortega <jose.sanchez.ortega@udla.edu.ec>
2024-07-04 07:15:57 -0500

    Agregar archivos de proyecto.

---
+++ Evaluacion3P/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Evaluacion3P
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Evaluacion3P/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Evaluacion3P
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
